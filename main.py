da57f6b3dab50d4b4b9fd588975b476dc0c2808e
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-12-01 13:30:10 +0200

    add cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/predict-price')
+@app.route('predict-price')
 def predictPrice():
     return 1
 
