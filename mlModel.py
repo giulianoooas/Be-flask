eb672370192fbf4c52103524911f5efbeda5c7ef
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-03-24 14:48:09 +0200

    add metrics validation

--- mlModel.py
+++ mlModel.py
@@ -3,6 +3,7 @@ from dataNormalization import BagOfWord, Vocabulary
 from tensorflow.keras import Sequential, Input
 from tensorflow.keras.layers import Dense, Dropout
 import numpy as np
+from sklearn.metrics import classification_report
 from tensorflow.keras.utils import to_categorical
 import json
 
@@ -23,11 +24,19 @@ class NeuralNetworkModel:
             self.dataModelation = BagOfWord(data, norm, max_features)
         else:
             self.dataModelation = Vocabulary(data,norm,max_features)
+
+        self.generateDatasets()
         self.generateModel()
         self.trainModel() 
        
 
-    
+    def generateDatasets(self):
+        n = int(len(self.dataModelation.labels) * 0.8)
+        self.trainX = self.dataModelation.matrix[:n]
+        self.trainY = self.dataModelation.labels[:n]
+        self.testX = self.dataModelation.matrix[n:]
+        self.testY = self.dataModelation.labels[n:]
+
     def generateModel(self): 
         self.model = Sequential()
         self.model.add(Input(shape=(self.dataModelation.n), batch_size=self.batch_size))
@@ -39,12 +48,20 @@ class NeuralNetworkModel:
     
     def trainModel(self):
         self.model.fit( 
-            self.dataModelation.matrix,
-            to_categorical(self.dataModelation.labels,2),
+            self.trainX,
+            to_categorical(self.trainY,2),
             epochs = self.epochs,
             batch_size = self.batch_size
         )
     
+    def testModel(self):
+        predictions = [np.argmax(i) for i in self.model.predict(self.testX)]
+        print(classification_report(self.testY, predictions))
+
     def predict(self, sentence):
         value = self.model.predict(self.dataModelation.transform([sentence]))
         return np.argmax(value[0])
+
+if __name__ == '__main__':
+    model = NeuralNetworkModel()
+    model.testModel()
