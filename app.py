15bae664b5a15161b95131f18ef4e52cc14d4960
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-03-28 12:20:28 +0300

    fix route

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
+from flask_cors import CORS
 import os
 from mlModel import MlModel
 
 # labels : 0 -> bad, 1 = good
 
 app = Flask(__name__, instance_relative_config=True)
+CORS(app)
 
 @app.route('/comment-status')
 def predictPrice():
-    comment = request.json['comment']
+    comment = request.args['comment']
     return str(mlModel.predict(comment)), 200
 
 if __name__ == '__main__':
