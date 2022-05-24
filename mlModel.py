a362bb2af29b5398288e2db815d413199d91828d
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-05-24 15:02:20 +0300

    change model

--- mlModel.py
+++ mlModel.py
@@ -16,6 +16,12 @@ class MlModel:
             norm = configs['norm']
             self.epochs = configs['epochs']
             vocabulary = configs['vocabulary']
+            self.brains = configs['brains']
+            if self.brains <= 0:
+                self.brains = 1
+            if self.brains & 1 == 0:
+                # mereu vreau numar impar de creierase
+                self.brains += 1
 
         data = None
         data = readData()
@@ -29,6 +35,15 @@ class MlModel:
         self.generateModel()
         self.trainModel() 
        
+    def getCreatedModel(self):
+        model = Sequential()
+        model.add(Input(shape=(self.dataModelation.n), batch_size=self.batch_size))
+        model.add(Dense(100, activation="relu"))
+        model.add( Dense(64, activation="relu"))
+        model.add(Dropout(0.2))
+        model.add(Dense(2, activation="softmax"))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
 
     def generateDatasets(self):
         n = int(len(self.dataModelation.labels) * 0.8)
@@ -38,29 +53,54 @@ class MlModel:
         self.testY = self.dataModelation.labels[n:]
 
     def generateModel(self): 
-        self.model = Sequential()
-        self.model.add(Input(shape=(self.dataModelation.n), batch_size=self.batch_size))
-        self.model.add(Dense(64, activation="relu"))
-        self.model.add( Dense(64, activation="relu"))
-        self.model.add(Dropout(0.2))
-        self.model.add(Dense(2, activation="softmax"))
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model = [self.getCreatedModel() for _ in range(self.brains)]
     
     def trainModel(self):
-        self.model.fit( 
-            self.trainX,
-            to_categorical(self.trainY,2),
-            epochs = self.epochs,
-            batch_size = self.batch_size
-        )
-    
-    def testModel(self):
-        predictions = [np.argmax(i) for i in self.model.predict(self.testX)]
-        print(classification_report(self.testY, predictions))
+        for model in self.model:
+            model.fit( 
+                self.trainX,
+                to_categorical(self.trainY,2),
+                epochs = self.epochs,
+                batch_size = self.batch_size
+            )
 
-    def predict(self, sentence):
-        value = self.model.predict(self.dataModelation.transform([sentence]))
-        return np.argmax(value[0])
+    def predict(self, value):
+        arr = [
+            np.argmax(
+                model.predict(
+                    self.dataModelation.transform([value])
+                    )
+                ) for model in self.model]
+
+        d = [0,0]
+        for i in arr:
+            d[i] += 1
+
+        if d[0] > d[1]:
+            return 0
+
+        return 1
+        
+
+
+    def testModel(self):
+        predictions = [[np.argmax(i) for i in model.predict(self.testX)] for model in self.model]
+        
+        n = len(self.testY)
+        d = [[0,0] for i in range(n)]
+
+        for i in range(n):
+            for j in range(self.brains):
+                d[i][predictions[j][i]] += 1
+
+        predictions = []
+        for val in d:
+            if val[0] > val[1]:
+                predictions.append(0)
+            else:
+                predictions.append(1)
+
+        print(classification_report(self.testY, predictions))
 
 
 if __name__ == '__main__':
